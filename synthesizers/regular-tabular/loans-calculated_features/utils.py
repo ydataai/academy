59751355613ab14ac92054e713d696bdfa09ba57
Francisco Santos <francisco.santos@ydata.ai>
2022-02-07 18:37:37 +0000

    transactions balance calculation example
    update example, remove previous dataset
    
    improve plots and text
    
    revisions
    
    delete synthesizer train log output
    
    revisions
    
    move files before rebase

---
+++ synthesizers/regular-tabular/loans-calculated_features/utils.py
@@ -0,0 +1,103 @@
+"Utility methods for plotting in the calculated features quickstart notebook."
+from typing import Union, Dict
+
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+import seaborn as sns
+import pandas as pd
+
+from ydata.dataset import Dataset
+
+def _viz_revol_util(ax, sample: pd.DataFrame):
+    "Plots the product of total credit limit and revolving credit utilization to the revolving credit balance."
+    expected_revol_bal = sample.revol_util * sample.total_rev_hi_lim
+    sns.scatterplot(x=expected_revol_bal, y=sample.revol_bal, s=150, ax=ax)
+    ax.set(xscale="log", yscale="log")
+    plt.setp(ax, xlabel = 'Predicted revolving balance, $')
+    plt.setp(ax, ylabel = 'Expected revolving balance, $')
+    lims = [min([ax.get_xlim(), ax.get_ylim()]), max([ax.get_xlim(), ax.get_ylim()])]
+    ax.plot(lims, lims, 'k', alpha=0.75)
+    ax.grid(False)
+    sns.despine(left= False, bottom=False)
+    ax.title.set_text(f'Revolving balance')
+
+def _viz_total_pymnt(ax, sample: pd.DataFrame):
+    "Compares the summation of the different pay components to the total payments."
+    payments = pd.DataFrame({'Interest': [sample.total_rec_int.mean(), 0],
+                            'Late fee': [sample.total_rec_late_fee.mean(), 0],
+                            'Principal': [sample.total_rec_prncp.mean(), 0],
+                            'Total Payment': [0, sample.total_pymnt.mean()]},
+                            index=['Components', 'Total Payment'])
+    payments.plot(kind='bar', stacked=True, edgecolor='k', linewidth=1.5, width=1.0, ax=ax)
+    ax.grid(False)
+    sns.despine(left= False, bottom=False)
+    plt.setp(ax, ylabel='Amount, $')
+    ax.title.set_text(f'Breakdown of payment components in the dataset (average)')
+
+def _viz_installment(ax, sample: pd.DataFrame):
+    """Compares the expected payment schedule with the proposed installments of a random record in the sample."""
+    record = sample.sample(n=1)
+    term = 36 if record.term.values == '36m' else 60
+    cumul_payed = [record.installment.values[0]*period for period in range(1,term+1)]
+    loan_amnt = record.loan_amnt.values[0]
+    p_interest = record.int_rate.values[0]/12  # Payed monthly
+    expected_cumul = term*(loan_amnt*((p_interest*(1+p_interest)**term)/((1+p_interest)**term-1)))
+    sns.barplot(ax = ax, x=list(range(1,term+1)), y=cumul_payed, color=ydata_colorlist()[0], edgecolor=ydata_colorlist()[0])
+    xlabels = [item.get_text() for item in ax.get_xticklabels()]
+    xlabels = [label if label in ['1', str(term)] else '' for label in xlabels]
+    ax.set_xticklabels(xlabels)
+    for patch in ax.patches:
+        patch.set_width(1)
+    ax.axhline(expected_cumul, label='Total due payment', color=ydata_colorlist()[1])
+    ax.title.set_text('Randomly sampled loan schedule: {}$ loan, {:.1f}% interest {} month term'.format(loan_amnt, p_interest*12*100, term))
+    ax.legend(loc='upper center')
+    plt.setp(ax, ylabel='Cumulative payments, $')
+    plt.setp(ax, xlabel = 'Time, months')
+
+def viz_main(sample: Union[Dataset, pd.DataFrame], sample_name: str = 'Real Data'):
+    if isinstance(sample, Dataset):
+        sample = sample.to_pandas()
+    fig, axs = plt.subplots(3, 1, constrained_layout=True, figsize=(12,16))
+    _viz_revol_util(axs[0], sample)
+    _viz_total_pymnt(axs[1], sample)
+    _viz_installment(axs[2], sample)
+    fig.suptitle(sample_name, fontweight='bold')
+    fig.tight_layout(pad=2)
+    plt.show()
+
+def viz_side_by_side(samples: Dict[str, Dataset]):
+    samples = {k:v.to_pandas() for k, v in samples.items()}
+    fig = plt.figure(figsize=(26,28))
+    gs = GridSpec(nrows=4,ncols=2, height_ratios = [0.0001,1,1,1])
+    axs = [[plt.subplot(gs[j, i]) for j in range(4)] for i in range(2)]
+    for i, (sample_name, sample) in enumerate(samples.items()):
+        axs[i][0].axis('off')
+        axs[i][0].set_title(sample_name, fontweight='bold')
+        _viz_revol_util(axs[i][1], sample)
+        _viz_total_pymnt(axs[i][2], sample)
+        _viz_installment(axs[i][3], sample)
+    fig.tight_layout(pad=0.5)
+    plt.show()
+
+
+def ydata_colorlist(n: int = None):
+    """Returns a colorlist with the YData colors.
+    Pass n to define a truncated color map (use less colors)"""
+    colors = ["#830000",  # Dark red
+              "#040404",  # Dominant hue
+              "#E32212",  # Accent color
+              "#474747",  # Subheadlines
+              "#FF6464",  # Error
+              "#FFEA2D",  # Warning
+              "#FFFFFF",  # Primary color
+              "#5FED5C",  # Success
+              "#1298E3",  # Visual aid
+              "#7A7A7A",  # Border dividers
+              "#B8B8B8",  # Disabled states
+              "#F5F5F5"]  # Backgrounds, text on dark background
+    if n and n>len(colors):
+        n=len(colors)
+    return colors[:n]
+
+sns.set_theme(style='whitegrid', palette=ydata_colorlist(), rc={"grid.color": ".4", "grid.linestyle": ":"})
+plt.rcParams["figure.figsize"] = (18,6)
